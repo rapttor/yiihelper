66f340a9da9920bdd2ef7da2e6f7d2a790f97d34
emir kurtovic <emir.kurtovic@toptal.com>
2018-06-30 22:11:28 +0200

    Helper

---
+++ Helper.php
@@ -0,0 +1,485 @@
+<?php
+
+$HELPER = array(
+    "debug" => array()
+);
+
+class Helper extends Controller
+{
+    public static function urlClean($str,$delimiter='-') {
+        $str=trim($str);
+        setlocale(LC_ALL, 'en_US.UTF8');
+        $clean = iconv('UTF-8', 'ASCII//TRANSLIT', $str);
+        $clean = preg_replace("/[^a-zA-Z0-9|+ -]/", '', $clean);
+        $clean = strtolower(trim($clean, '-'));
+        $clean = preg_replace("/[|+ -]+/", $delimiter, $clean);
+        return $clean;
+    }
+
+    public static function showdebug()
+    {
+        global $HELPER;
+        echo json_encode($HELPER);
+    }
+
+    public static function debug($message, $type = "info", $value = null)
+    {
+        global $HELPER;
+        $debug = array("message" => $message, "type" => $type, "value" => $value);
+        $HELPER["debug"][] = $debug;
+        $message = serialize($debug);
+        error_log($message);
+    }
+
+    public static function header($title, $icon = null)
+    {
+        return '<h1 class="pull-right"><i class="icons icon-' . $icon . ' pull-right"></i>
+            ' . Yii::t("main", $title) . ' &nbsp;</h1>';
+    }
+
+    public static function checkEmail($mail)
+    {
+        $disposable_mail = file_get_contents(Yii::app()->getBasePath() . "/../protected/modules/email/disposable-email.csv");
+        $disposable_mail = explode(",", $disposable_mail);
+        foreach ($disposable_mail as $disposable) {
+            list(, $mail_domain) = explode('@', $mail);
+            if (strcasecmp($mail_domain, $disposable) == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static function imgurl($cat = null, $id = null)
+    {
+        $base = Yii::app()->baseUrl;
+        return $base . "/uploads/" . $cat . "/" . $id . ".jpg";
+    }
+
+    public static function img($cat = null, $id = null, $class = "img-responsive")
+    {
+        return "<img src='" . self::imgurl($cat, $id) . "'  class='$class'>";
+    }
+
+    public static function uploadDir($cat = null)
+    {
+        if (isset($_SERVER["HTTP_HOST"])) {
+            $base = $_SERVER['DOCUMENT_ROOT'] . "/" . Yii::app()->baseUrl;
+        } else {
+            $base = Yii::getPathOfAlias('application');
+        }
+        return $base . "/uploads/" . (is_null($cat) ? "" : $cat . '/');
+    }
+
+    public static function arrayValue($a, $i, $default = null)
+    {
+        return (is_array($a) && isset($a[$i])) ? $a[$i] : $default;
+    }
+
+    public static function adLength($i)
+    {
+        return self::arrayValue(Yii::app()->params["ad"]["length"], $i);
+    }
+
+    public static function rand_date($min_date = "01-01-2016", $max_date = "31-12-2016")
+    {
+        /* Gets 2 dates as string, earlier and later date.
+           Returns date in between them.
+        */
+
+        $min_epoch = strtotime($min_date);
+        $max_epoch = strtotime($max_date);
+
+        $rand_epoch = rand($min_epoch, $max_epoch);
+
+        return date('Y-m-d H:i:s', $rand_epoch);
+    }
+
+    public static function domain($str, $dom = "")
+    {
+        return (strpos($str, "http") === false) ? "http://" . $str : $str;
+    }
+
+    public static function link($url, $text = null, $options = 'target="_blank"')
+    {
+        if (is_null($text)) $text = $url;
+        $link = self::domain($url);
+        return "<a href='$link' $options>$text</a>";
+    }
+
+    public static function time_elapsed_string($datetime, $full = false)
+    {
+        $now = new DateTime;
+        $ago = new DateTime($datetime, new DateTimeZone(date_default_timezone_get()));
+        $diff = $now->diff($ago);
+
+        $diff->w = floor($diff->d / 7);
+        $diff->d -= $diff->w * 7;
+
+        $string = array(
+            'y' => 'year',
+            'm' => 'month',
+            'w' => 'week',
+            'd' => 'day',
+            'h' => 'hour',
+            'i' => 'minute',
+            's' => 'second',
+        );
+        foreach ($string as $k => &$v) {
+            if ($diff->$k) {
+                $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+            } else {
+                unset($string[$k]);
+            }
+        }
+
+        if (!$full) $string = array_slice($string, 0, 1);
+        return $string ? implode(', ', $string) . ' ago' : 'just now';
+    }
+
+    public static function ago($tm, $rcs = 0)
+    {
+        if (is_string($tm)) $tm = strtotime($tm);
+        $cur_tm = time();
+        $dif = $cur_tm - $tm;
+        $pds = array('second', 'minute', 'hour', 'day', 'week', 'month', 'year', 'decade');
+        $lngh = array(1, 60, 3600, 86400, 604800, 2630880, 31570560, 315705600);
+
+        for ($v = sizeof($lngh) - 1; ($v >= 0) && (($no = $dif / $lngh[$v]) <= 1); $v--) ;
+        if ($v < 0) $v = 0;
+        $_tm = $cur_tm - ($dif % $lngh[$v]);
+        $no = floor($no);
+        if ($no <> 1)
+            $pds[$v] .= 's';
+        $x = sprintf("%d %s ", $no, $pds[$v]);
+        if (($rcs == 1) && ($v >= 1) && (($cur_tm - $_tm) > 0))
+            $x .= self::ago($_tm);
+        return $x;
+    }
+
+    public static function more($str, $length = 200, $more = "<!-- more -->")
+    {
+        if (strlen($str) < $length)
+            return $str;
+
+        $id = "SH" . sha1($str);
+        $length = (strpos($str, $more) !== false) ? strpos($str, $more) : $length;
+
+        return "<div id='$id'><div class='excerpt'>" . substr($str, 0, $length) . "</div><div style='display:none;' class='more'>" . substr($str, $length, strlen($str)) . "</div>
+        </div><a href='javascript:;' title='$length / " . strlen($str) . "' style='cursor:pointer;' onclick='$(\"#$id .more\").toggle();'>[...]</a>";
+    }
+
+    public static function migrate($def, $db)
+    {
+        foreach ($def as $n => $t) {
+            $n = trim($n);
+            if (!isset($t["fields"]) && !isset($t["index"]))
+                $t["fields"] = $t; // set up fields from whole array
+            @$db->createTable(trim($n), $t["fields"]); // creating a table
+            if (isset($t["index"])) foreach ($t["index"] as $u => $f)
+                if (!is_null($f)) foreach ($f as $field)
+                    @$db->createIndex(trim($n) . $field, trim($n), $field, $u); // create indexe's
+
+            //if (defined("DEVELOPMENT") && DEVELOPMENT)
+            if (isset($t["values"])) foreach ($t["values"] as $u => $f) {
+                if (is_numeric($u)) $db->insert(trim($n), $f); // create initial values
+            }
+            if (isset($t["titlevalues"])) foreach ($t["titlevalues"] as $u => $f) {
+                $i = array("title" => $u);
+                @$db->insert($n, $i);
+                $pid = Yii::app()->db->getLastInsertId();
+                foreach ($f as $i) {
+                    $tv = array("parent_id" => $pid, "title" => trim($i));
+                    @$db->insert($n, $tv);
+                }
+
+            }
+        }
+    }
+
+    public static function IconMenu($menu)
+    {
+        $result = "";
+        foreach ($menu as $m) $result .= self::Icon($m);
+        return $result;
+    }
+
+    public static function Icon($i)
+    {
+        $result = "";
+        if (!isset($i["value"]) && isset($i["url"])) $i["value"] = $i["url"];
+        if (isset($i["value"]) && isset($i["ion"]) && isset($i["title"])) $result = "<div class='icontext' onclick='window.location.href=\"" . Yii::app()->createUrl($i["value"]) . "\"'>
+        <i class='{$i["ion"]}'></i>
+        <small>{$i["title"]}</small>
+        </div>";
+
+        return $result;
+    }
+
+    public static function aVal($a, $k, $d = "")
+    {
+        return (is_array($a) && isset($a[$k])) ? $a[$k] : $d;
+
+    }
+
+    public static function back()
+    {
+        return "<div class='clearfix'></div><a style='clear:both;margin:10px 0;' class='btn btn-primary' onclick='history.go(-1);'><i class='fa fa-caret-left'></i> " . Yii::t("main", "Back") . "</a><div class='clearfix'></div>";
+    }
+
+    public static function is_json($string)
+    {
+        return ((is_string($string) &&
+            (is_object(json_decode($string)) ||
+                is_array(json_decode($string))))) ? true : false;
+    }
+
+    public static function json_validate($string)
+    {
+        // decode the JSON data
+        $result = json_decode($string);
+
+        // switch and check possible JSON errors
+        switch (json_last_error()) {
+            case JSON_ERROR_NONE:
+                $error = ''; // JSON is valid // No error has occurred
+                break;
+            case JSON_ERROR_DEPTH:
+                $error = 'The maximum stack depth has been exceeded.';
+                break;
+            case JSON_ERROR_STATE_MISMATCH:
+                $error = 'Invalid or malformed JSON.';
+                break;
+            case JSON_ERROR_CTRL_CHAR:
+                $error = 'Control character error, possibly incorrectly encoded.';
+                break;
+            case JSON_ERROR_SYNTAX:
+                $error = 'Syntax error, malformed JSON.';
+                break;
+            // PHP >= 5.3.3
+            case JSON_ERROR_UTF8:
+                $error = 'Malformed UTF-8 characters, possibly incorrectly encoded.';
+                break;
+            // PHP >= 5.5.0
+            case JSON_ERROR_RECURSION:
+                $error = 'One or more recursive references in the value to be encoded.';
+                break;
+            // PHP >= 5.5.0
+            case JSON_ERROR_INF_OR_NAN:
+                $error = 'One or more NAN or INF values in the value to be encoded.';
+                break;
+            case JSON_ERROR_UNSUPPORTED_TYPE:
+                $error = 'A value of a type that cannot be encoded was given.';
+                break;
+            default:
+                $error = 'Unknown JSON error occured.';
+                break;
+        }
+
+        if ($error !== '') {
+            // throw the Exception or exit // or whatever :)
+            exit($error);
+        }
+
+        // everything is OK
+        return $result;
+    }
+
+    public static function jsonValue($arr, $key, $def)
+    {
+        $value = $def;
+        if (isset($arr[$key]) && strlen(trim(strip_tags($arr[$key]))) > 2) $value = json_decode($arr[$key]);
+        return $value;
+    }
+
+    public static function jsonError()
+    {
+        $error = null;
+        switch (json_last_error()) {
+            case JSON_ERROR_NONE:
+                $error = ''; // JSON is valid // No error has occurred
+                break;
+            case JSON_ERROR_DEPTH:
+                $error = 'The maximum stack depth has been exceeded.';
+                break;
+            case JSON_ERROR_STATE_MISMATCH:
+                $error = 'Invalid or malformed JSON.';
+                break;
+            case JSON_ERROR_CTRL_CHAR:
+                $error = 'Control character error, possibly incorrectly encoded.';
+                break;
+            case JSON_ERROR_SYNTAX:
+                $error = 'Syntax error, malformed JSON.';
+                break;
+            // PHP >= 5.3.3
+            case JSON_ERROR_UTF8:
+                $error = 'Malformed UTF-8 characters, possibly incorrectly encoded.';
+                break;
+            // PHP >= 5.5.0
+            case JSON_ERROR_RECURSION:
+                $error = 'One or more recursive references in the value to be encoded.';
+                break;
+            // PHP >= 5.5.0
+            case JSON_ERROR_INF_OR_NAN:
+                $error = 'One or more NAN or INF values in the value to be encoded.';
+                break;
+            case JSON_ERROR_UNSUPPORTED_TYPE:
+                $error = 'A value of a type that cannot be encoded was given.';
+                break;
+            default:
+                $error = 'Unknown JSON error occured.';
+                break;
+        }
+        return $error;
+    }
+
+    public static function send($data, $cache = false, $die = true)
+    {
+        $json = (!self::is_json($data)) ? json_encode($data) : $data;
+        if (!$cache) {
+            header('Cache-Control: no-cache, must-revalidate');
+            header('Expires: Mon, 1 Avg 1999 05:00:00 GMT');
+        }
+        header('Content-Type: application/json');
+        echo $json;
+        foreach (Yii::app()->log->routes as $route) {
+            if ($route instanceof CWebLogRoute) {
+                $route->enabled = false; // disable any weblogroutes
+            }
+        }
+        if ($die) Yii::app()->end();
+    }
+
+    public static function map($str, $params)
+    {
+        foreach ($params as $key => $value)
+            $str = str_replace($key, $value, $str);
+        return $str;
+    }
+
+    public static function receive($url, $params = null, $cache = true)
+    { // receive json
+        if (!is_null($params)) $url = self::map($url, $params);
+        $data = false;
+        if ($cache) {
+            $data = Yii::app()->cache->get($url);
+        }
+        if ($data === false || isset($_GET["nocache"])) {
+            $data = self::curl($url);
+            if (isset($_GET["debug"])) {
+                $size = strlen($data);
+                echo "from scratch: $url $size <br/>";
+            }
+        }
+        if ($cache && $data && strlen($data) > 0) {
+            Yii::app()->cache->set($url, $data, CACHETIME);
+        }
+        return $data;
+    }
+
+    public static function curl($url)
+    {
+        if (!function_exists('curl_version')) {
+            exit ("Enable cURL in PHP");
+        }
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        $data = curl_exec($ch);
+        $error = "";
+        if (isset($_GET["debug"]))
+            $error = 'Curl error: ' . curl_error($ch);
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        if ($httpcode >= 200 && $httpcode < 300 && $data) {
+            return $data;
+        } else {
+            error_log($error);
+            return $error;
+        }
+    }
+
+    public static function exportModelAsJson($data)
+    {
+        return $json = (!self::is_json($data)) ? json_encode($data) : $data;
+    }
+
+    public static function ellipsis($text, $length)
+    {
+        return (mb_strlen($text) > $length) ? mb_substr($text, 0, $length) . '... ' : $text;
+    }
+
+    public static function replaceAll($what, $with, $str)
+    {
+        while (stripos($str, $what)) $str = str_ireplace($what, $with, $str);
+        return $str;
+    }
+
+    public static function urlText($str)
+    {
+        return self::replaceAll('__', '_', preg_replace('/[^\w]/', '_', $str));
+
+    }
+
+    public static function cors()
+    {
+
+        // Allow from any origin
+        if (isset($_SERVER['HTTP_ORIGIN'])) {
+            // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+            // you want to allow, and if so:
+            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Max-Age: 86400');    // cache for 1 day
+        }
+
+        // Access-Control headers are received during OPTIONS requests
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+                // may also be using PUT, PATCH, HEAD etc
+                header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+            exit(0);
+        }
+    }
+
+    public static function sortItems($array, $key, $desc = false)
+    {
+        $sorter = array();
+        $ret = array();
+        reset($array);
+        foreach ($array as $ii => $va) {
+            $sorter[$ii] = $va[$key];
+        }
+        asort($sorter);
+        if ($desc) $sorter = array_reverse($sorter);
+        foreach ($sorter as $ii => $va) {
+            $ret[$ii] = $array[$ii];
+        }
+        $array = $ret;
+
+        return $array;
+    }
+
+    public static function str2arr($str)
+    {
+        $arr = array();
+        if (is_string($str)) $arr = explode(',', $str);
+        foreach ($arr as $key => $value) $arr[$key] = trim($value);
+        $arr = array_unique($arr);
+        return $arr;
+    }
+
+    public static function vardumper($object){
+        echo "<pre>";
+        var_dump($object);
+        die;
+    }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+**Helper class**
+
+*Helper Class*
+    `General helper class`
+    
+*Y*
+    `Yii helper class`
