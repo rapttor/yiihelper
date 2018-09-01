85c07e375fc7d63119e99d53dc62d118457b6438
emir kurtovic <emir.kurtovic@toptal.com>
2018-09-01 20:01:16 +0200

    Console

--- src/Helper.php
+++ src/Helper.php
@@ -690,7 +690,7 @@ class Helper extends \Controller
 
         ini_set('memory_limit', '-1');
         $commandPath = \Yii::app()->getBasePath() . DIRECTORY_SEPARATOR . 'commands';
-        $runner = new CConsoleCommandRunner();
+        $runner = new \CConsoleCommandRunner();
         $runner->addCommands($commandPath);
         $commandPath = \Yii::getFrameworkPath() . DIRECTORY_SEPARATOR . 'cli' . DIRECTORY_SEPARATOR . 'commands';
         $runner->addCommands($commandPath);
